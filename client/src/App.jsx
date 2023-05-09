3754071da1faac6c8d9d1497c4677c5ef6655c00
Derlis Cuba <d.erliscuba14@hotmail.com>
2023-05-09 13:48:07 -0400

    Derlis: Directory fixed, Added Eslint and Prettier config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,9 @@ import './App.css'
 import RegistroPacientes from './Pages/RegistroPacientes'
 
 function App() {
-
   return (
     <>
-      <RegistroPacientes/>
+      <RegistroPacientes />
     </>
   )
 }
